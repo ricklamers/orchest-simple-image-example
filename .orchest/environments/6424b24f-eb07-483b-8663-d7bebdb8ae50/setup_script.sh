482144b08f230edc2e566f9bd39dbaad1aecf694
Rick Lamers <ricklamers@gmail.com>
2022-05-09 13:25:24 +0000

    Init

---
+++ .orchest/environments/6424b24f-eb07-483b-8663-d7bebdb8ae50/setup_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install any dependencies you have in this shell script,
+# see https://docs.orchest.io/en/latest/fundamentals/environments.html#install-packages
+
+# E.g. mamba install -y tensorflow
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install python3-opencv -y
+pip install Pillow requests matplotlib scikit-learn opencv-python
